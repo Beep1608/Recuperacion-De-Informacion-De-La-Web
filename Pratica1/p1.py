7cac73477af067cb5caba034bb84fc911313263e
Fidel Zarco <fidel.zarco1608@gmail.com>
2025-01-30 08:15:36 -0600

    Practica 1 finalizada

--- Pratica1/p1.py
+++ Pratica1/p1.py
@@ -23,7 +23,39 @@ def indexInfo():
     print('Indexación terminada')
 
 
+
+
+def searchInfo(texto):
+    with open('index.dat','rb') as file:
+
+        while True:
+            len_name_bytes = file.read(4)
+            if not len_name_bytes:
+                break
+            len_name = struct.unpack('I', len_name_bytes)[0]
+            name_prod =  file.read(len_name).decode('utf-8')
+
+            pos_bytes = file.read(8)
+            pos = struct.unpack('Q',pos_bytes)[0]
+
+            if name_prod.lower() == texto.lower():
+                with open('inventario.txt','r') as data:
+                    data.seek(pos)
+                    info = data.readline().split('-')
+                    name= info[0]
+                    quantity = info[1]
+                    price = info[2]
+                    string = f"""Producto : {name} , Cantidad : {quantity} , Precio : {price}"""
+                   
+                    print(info)
+                    st.write(string )
+                    return
+                 
+        st.write("""Producto no encontrado""")
+
+
 i = indexInfo
+
 
 #TODO: Implementar la buscar en el .dat
 
@@ -34,6 +66,6 @@ st.write(
 """
 )
 
-st.text_input("Buscar", on_change=i)
+text_input = st.text_input("Buscar", on_change=i) 
 
-
+searchInfo(text_input)
